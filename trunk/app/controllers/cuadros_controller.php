562e9d2fff31989db76cf329a388527f5bec9a50
laguerre1206 <laguerre1206@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2009-11-27 14:48:22 +0000

    Se modificó la query del cuadro de total de instituciones.

--- trunk/app/controllers/cuadros_controller.php
+++ trunk/app/controllers/cuadros_controller.php
@@ -6,40 +6,36 @@ class CuadrosController extends AppController {
 	
 	function total_instits_por_gestion_jurisdiccion() {
 		$this->CustomQuery->sql = "
-				(select
-				    j.name as \"División politíco-territorial\" ,
-				    sum(CASE WHEN ( i.gestion_id = 1 ) THEN 1 ELSE 0 END )as \"Gestión Estatal\" ,
-				    sum(CASE WHEN ( i.gestion_id = 2 ) THEN 1 ELSE 0 END )as \"Gestión Privada\" ,
-				    count(*) as \"Total\"
-				from instits i
-				left join jurisdicciones j on j.id = i.jurisdiccion_id
-				where i.activo = 1
-				AND i.dependencia_id = 1
-				group by
-				    j.name
-				order by
-				    j.name
-				)
-				union all
-				(
-				select
-				    cast ( 'Universidades Nacionales' as varchar(40)) as \"División politíco-territorial\" ,
-				    count(*) as \"Gestión Estatal\",
-				    0 as \"Gestión Privada\" ,
-				    count(*) as \"Total\"
-				from instits i
-				where  i.dependencia_id = 2
-				AND i.activo = 1
-				)
-				union all
-				(
-				select 
-				    cast ( 'Total' as varchar(40)) as \"División politíco-territorial\" ,
-				    sum(CASE WHEN ( i.gestion_id = 1 OR  i.dependencia_id = 2 ) THEN 1 ELSE 0 END ) as \"Gestión Estatal\" ,
-				    sum(CASE WHEN ( i.gestion_id = 2 ) THEN 1 ELSE 0 END )as \"Gestión Privada\" ,
-				    count(*) as \"Total\"
-				from instits i
-				where i.activo = 1)";		
+				(SELECT j.name as \"División politíco-territorial\" ,
+	                    sum(CASE WHEN (i.gestion_id = 1 ) THEN 1 ELSE 0 END )as \"Gestión Estatal\" ,
+	                    sum(CASE WHEN (i.gestion_id = 2 ) THEN 1 ELSE 0 END )as \"Gestión Privada\" ,
+                        count(*) as \"Total\"
+                 FROM   instits i LEFT JOIN jurisdicciones j ON j.id = i.jurisdiccion_id
+                 WHERE  i.activo = 1
+                 AND    i.dependencia_id <> 2
+                 GROUP BY j.name
+                 ORDER BY j.name
+                )
+                UNION ALL
+                (
+                 SELECT cast ('Universidades Nacionales' as varchar(40)) as \"División politíco-territorial\",
+	                    sum(CASE WHEN (i.gestion_id = 1) THEN 1 ELSE 0 END )as \"Gestión Estatal\" ,
+	                    sum(CASE WHEN (i.gestion_id = 2) THEN 1 ELSE 0 END )as \"Gestión Privada\" ,
+                        count(*) as \"Total\"
+                 FROM  instits i
+                 WHERE i.dependencia_id = 2
+                 AND   i.activo = 1
+                )
+                UNION ALL
+                (
+                 SELECT cast ( 'Total' as varchar(40)) as \"División politíco-territorial\" ,
+                 sum(CASE WHEN (i.gestion_id = 1) THEN 1 ELSE 0 END ) as \"Gestión Estatal\" ,
+                 sum(CASE WHEN (i.gestion_id = 2) THEN 1 ELSE 0 END )as \"Gestión Privada\",
+                 count(*) as \"Total\"
+                 FROM   instits i 
+                 WHERE  i.activo = 1
+                )	
+              ";
 		
 		//$data = $this->paginate($this->CustomQuery);
 		$data = $this->CustomQuery->query();
