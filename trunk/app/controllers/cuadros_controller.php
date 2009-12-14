d3bd9841a4532bb12df9695b48c95b12b8c551ae
laguerre1206 <laguerre1206@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2009-12-14 14:30:00 +0000

    Modificacion del query del cuadro

--- trunk/app/controllers/cuadros_controller.php
+++ trunk/app/controllers/cuadros_controller.php
@@ -6,36 +6,80 @@ class CuadrosController extends AppController {
 	
 	function total_instits_por_gestion_jurisdiccion() {
 		$this->CustomQuery->sql = "
-				(SELECT j.name as \"División político-territorial\" ,
-	                    sum(CASE WHEN (i.gestion_id = 1 ) THEN 1 ELSE 0 END )as \"Estatal\" ,
-	                    sum(CASE WHEN (i.gestion_id = 2 ) THEN 1 ELSE 0 END )as \"Privada\" ,
-                        count(*) as \"Total\"
-                 FROM   instits i LEFT JOIN jurisdicciones j ON j.id = i.jurisdiccion_id
-                 WHERE  i.activo = 1
-                 AND    i.dependencia_id <> 2
-                 GROUP BY j.name
-                 ORDER BY j.name
-                )
-                UNION ALL
-                (
-                 SELECT cast ('Universidades Nacionales' as varchar(40)) as \"División político-territorial\",
-	                    sum(CASE WHEN (i.gestion_id = 1) THEN 1 ELSE 0 END )as \"Estatal\" ,
-	                    sum(CASE WHEN (i.gestion_id = 2) THEN 1 ELSE 0 END )as \"Privada\" ,
-                        count(*) as \"Total\"
-                 FROM  instits i
-                 WHERE i.dependencia_id = 2
-                 AND   i.activo = 1
-                )
-                UNION ALL
-                (
+(
+	SELECT j.name as \"División político-territorial\" ,
+               sum(CASE WHEN (i.gestion_id = 1 ) THEN 1 ELSE 0 END )as \"Estatal\" ,
+ 	       sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END )as \"Estatal_FP\" ,
+ 	       sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END )as \"Estatal_SEC\",
+ 	       sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END )as \"Estatal_SUP\",
+ 	       sum(CASE WHEN (i.gestion_id = 1 AND i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"Estatal_ETP\",
+
+               sum(CASE WHEN (i.gestion_id = 2 ) THEN 1 ELSE 0 END )as \"Privada\" ,
+	       sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Privada_FP\" ,
+ 	       sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Privada_SEC\",
+ 	       sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Privada_SUP\",
+ 	       sum(CASE WHEN (i.gestion_id = 2 AND i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"Privada_ETP\",
+
+ 	       sum(CASE WHEN (i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_FP\" ,
+ 	       sum(CASE WHEN (i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_SEC\",
+ 	       sum(CASE WHEN (i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_SUP\",
+ 	       sum(CASE WHEN (i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"TOT_ETP\",
+               count(*) as \"Total\"
+        FROM   instits i LEFT JOIN jurisdicciones j ON j.id = i.jurisdiccion_id
+        WHERE  i.activo = 1
+        AND    i.dependencia_id <> 2
+        GROUP BY j.name
+        ORDER BY j.name
+        )
+        UNION ALL
+        (
+         SELECT cast ('Universidades Nacionales' as varchar(40)) as \"División político-territorial\",
+                sum(CASE WHEN (i.gestion_id = 1) THEN 1 ELSE 0 END )as \"Estatal\" ,
+  	        sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Estatal_FP\" ,
+ 	        sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Estatal_SEC\",
+ 	        sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Estatal_SUP\",
+ 	        sum(CASE WHEN (i.gestion_id = 1 AND i.etp_estado_id  = 1 ) THEN 1 ELSE 0 END ) as \"Estatal_ETP\",
+
+            sum(CASE WHEN (i.gestion_id = 2) THEN 1 ELSE 0 END )as \"Privada\" ,
+ 	        sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Privada_FP\" ,
+ 	        sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Privada_SEC\",
+                sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Privada_SUP\",
+                sum(CASE WHEN (i.gestion_id = 2 AND i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"Privada_ETP\",
+
+ 	       sum(CASE WHEN (i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_FP\" ,
+ 	       sum(CASE WHEN (i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_SEC\",
+ 	       sum(CASE WHEN (i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_SUP\",
+ 	       sum(CASE WHEN (i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"TOT_ETP\",
+                count(*) as \"Total\"
+         FROM  instits i
+         WHERE i.dependencia_id = 2
+         AND   i.activo = 1
+        )
+        UNION ALL
+        (
                  SELECT cast ( 'Total' as varchar(40)) as \"División político-territorial\" ,
                  sum(CASE WHEN (i.gestion_id = 1) THEN 1 ELSE 0 END ) as \"Estatal\" ,
-                 sum(CASE WHEN (i.gestion_id = 2) THEN 1 ELSE 0 END )as \"Privada\",
+    	         sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Estatal_FP\" ,
+ 	         sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Estatal_SEC\",
+ 	         sum(CASE WHEN (i.gestion_id = 1 AND i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"Estatal_SUP\",
+ 	         sum(CASE WHEN (i.gestion_id = 1 AND i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"Estatal_ETP\",
+
+
+                 sum(CASE WHEN (i.gestion_id = 2) THEN 1 ELSE 0 END ) as \"Privada\",
+ 	         sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END )as \"Privada_FP\" ,
+ 	         sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END )as \"Privada_SEC\",
+                 sum(CASE WHEN (i.gestion_id = 2 AND i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END )as \"Privada_SUP\",
+                 sum(CASE WHEN (i.gestion_id = 2 AND i.etp_estado_id  = 1 ) THEN 1 ELSE 0 END ) as \"Privada_ETP\",
+
+ 	         sum(CASE WHEN (i.claseinstit_id = 1 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_FP\" ,
+ 	         sum(CASE WHEN (i.claseinstit_id = 3 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_SEC\",
+ 	         sum(CASE WHEN (i.claseinstit_id = 4 AND i.etp_estado_id  = 2) THEN 1 ELSE 0 END ) as \"TOT_SUP\",
+ 	         sum(CASE WHEN (i.etp_estado_id  = 1) THEN 1 ELSE 0 END ) as \"TOT_ETP\",
                  count(*) as \"Total\"
                  FROM   instits i 
                  WHERE  i.activo = 1
                 )	
-              ";
+		              ";
 		
 		//$data = $this->paginate($this->CustomQuery);
 		$data = $this->CustomQuery->query();
