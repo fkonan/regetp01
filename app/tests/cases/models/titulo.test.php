bddcc092538f4b2deb9f27e5c3300098a455aac8
leandro.walker <leandro.walker@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-03-16 15:41:38 +0000

    Titulos de referencia y Orientaciones (TDD)

---
+++ app/tests/cases/models/titulo.test.php
@@ -0,0 +1,38 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* Titulo Test cases generated on: 2010-03-16 11:03:42 : 1268749422*/
+App::import('Model', 'Titulo');
+
+class TestTitulo extends Titulo {
+	var $cacheSources = false;
+	var $useDbConfig  = 'test_suite';
+}
+
+class TituloTestCase extends CakeTestCase {
+	var $Titulo = null;
+	var $fixtures = array('app.titulo', 'app.oferta');
+
+	function start() {
+		parent::start();
+		$this->Titulo = new TestTitulo();
+	}
+
+	function testTituloInstance() {
+		$this->assertTrue(is_a($this->Titulo, 'Titulo'));
+	}
+
+	function testTituloFind() {
+		$this->Titulo->recursive = -1;
+		$results = $this->Titulo->find('first');
+		$this->assertTrue(!empty($results));
+
+		$expected = array('Titulo' => array(
+			'id'  => 1,
+			'name'  => 'Lorem ipsum dolor sit amet',
+			'marcoref'  => 1,
+			'oferta_id'  => 1
+			));
+		$this->assertEqual($results, $expected);
+	}
+}
+?>
