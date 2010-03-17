5e741650fae3db82fea09e890155429febd47633
alevilar <alevilar@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-03-17 14:03:00 +0000

    test de orientacion
    agregada la orientacion y el subsector en el buscador de instituciones

--- app/models/orientacion.php
+++ app/models/orientacion.php
@@ -1,35 +1,36 @@
 <?php
 class Orientacion extends AppModel {
 
-	var $name = 'Orientacion';
+    var $name = 'Orientacion';
+    var $order = 'Orientacion.name';
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-	var $hasMany = array(
-			'Sector' => array('className' => 'Sector',
-								'foreignKey' => 'orientacion_id',
-								'dependent' => false,
-								'conditions' => '',
-								'fields' => '',
-								'order' => '',
-								'limit' => '',
-								'offset' => '',
-								'exclusive' => '',
-								'finderQuery' => '',
-								'counterQuery' => ''
-			),
-			'Instit' => array('className' => 'Instit',
-								'foreignKey' => 'orientacion_id',
-								'dependent' => false,
-								'conditions' => '',
-								'fields' => '',
-								'order' => '',
-								'limit' => '',
-								'offset' => '',
-								'exclusive' => '',
-								'finderQuery' => '',
-								'counterQuery' => ''
-			)
-	);
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
+    var $hasMany = array(
+            'Sector' => array('className' => 'Sector',
+                            'foreignKey' => 'orientacion_id',
+                            'dependent' => false,
+                            'conditions' => '',
+                            'fields' => '',
+                            'order' => '',
+                            'limit' => '',
+                            'offset' => '',
+                            'exclusive' => '',
+                            'finderQuery' => '',
+                            'counterQuery' => ''
+            ),
+            'Instit' => array('className' => 'Instit',
+                            'foreignKey' => 'orientacion_id',
+                            'dependent' => false,
+                            'conditions' => '',
+                            'fields' => '',
+                            'order' => '',
+                            'limit' => '',
+                            'offset' => '',
+                            'exclusive' => '',
+                            'finderQuery' => '',
+                            'counterQuery' => ''
+            )
+    );
 
 }
 ?>
--- app/tests/cases/models/instit.test.php
+++ app/tests/cases/models/instit.test.php
@@ -3,9 +3,9 @@
 /* Instit Test cases generated on: 2009-09-17 10:09:16 : 1253194756*/
 App::import('Model', 'Instit');
 
-class TestInstit extends Instit {
 
-	//var $cacheSources = false;
+class InstitTest extends Instit {
+    var $useDbConfig = 'test';
 }
 
 class InstitTestCase extends CakeTestCase {
@@ -15,100 +15,143 @@ class InstitTestCase extends CakeTestCase {
 		'app.instit', 'app.plan', 'app.sector', 'app.subsector', 'app.orientacion'
 	);
 	 
-	function start() 
-	{
-		parent::start();
-		$this->Instit = new Instit();
-	}
-	
-	function testGuardar()
-	{
-		//creo un plan mixto para la institucion 1
-		$plan = array(
-	    		//'id' 			=> 0,
-				'instit_id'		=> 1,
-				'oferta_id'		=> 1,
-				'old_item'		=> 0,
-				'norma'			=> "",
-				'nombre'		=> "Titulo nuevo",
-				'perfil'		=> "Algun Perfil",
-				'sector'		=> "",
-				'duracion_hs'	=> 3,
-				'duracion_semanas'=>2,
-				'duracion_anios'=> 4,
-				'matricula'		=> 100,
-				'observacion'	=> "Alguna observacion 1",
-				'ciclo_alta'	=> 2007,
-				'ciclo_mod'		=> 0,
-				//'created' 		=> "2010-01-21 13:54:45",
-				//'modified' 		=> "2010-01-21 13:54:45",
-				'sector_id'		=> 3, // Sector = Agropecuaria -> Orientacion = Agropecuaria
-				'subsector_id'	=> 2,
-    	);
-    	
-    	$orien = array('name'=> "Agropecuaria 2");
-    	$result = $this->Instit->Plan->Sector->Orientacion->save($orien);
-    	$this->Instit->Plan->Sector->Orientacion->recursive = -1;
-    	$this->assertTrue($result);
-		$lastInsertId = $this->Instit->Plan->Sector->Orientacion->getLastInsertID();
-		$this->assertTrue($lastInsertId != null);
-				
-//    	$this->Instit->Plan->recursive = -1;
-//    	debug($this->Instit->Plan->find('count'));
-//		$this->Instit->Plan->save($plan);
-//		debug($this->Instit->Plan->id);
-//		debug($this->Instit->Plan->find('count'));
-//		
-//		//debug($this->Instit->find('all',array('conditions'=>array('Instit.id'=>1))));
-//		$orientacion = $this->Instit->getOrientacionSegunSusPlanes(1);	
-//		$this->assertEqual($orientacion, 0); //orientacion otros
-		
-	
-	}
-	
-	function testGetOrientacionSegunSusPlanes() 
-	{    		
-		$this->loadFixtures('Instit', 'Plan', 'Sector', 'Subsector', 'Orientacion');
-		
-		$orientacion = $this->Instit->getOrientacionSegunSusPlanes(1);
-		$this->assertEqual($orientacion, 3); //orientacion otros
-		
-		$orientacion = $this->Instit->getOrientacionSegunSusPlanes(2);
-		$this->assertEqual($orientacion, 3);
-		
-		$orientacion = $this->Instit->getOrientacionSegunSusPlanes(3);
-		$this->assertEqual($orientacion, 2);
-		
-		$orientacion = $this->Instit->getOrientacionSegunSusPlanes(4);
-		$this->assertEqual($orientacion, 1);
-	}
-	
+    function start() {
+        parent::start();
+        $this->Instit =& ClassRegistry::init('Instit');
+    }
 
-	
-	function testIsCUEValid(){
-		// casos que deberian dar 1, porque todo paso bien
-		$this->assertEqual($this->Instit->isCUEValid("601254"),1);
-		$this->assertEqual($this->Instit->isCUEValid("201254"),1);
-		$this->assertEqual($this->Instit->isCUEValid("0601254"),1);
-		$this->assertEqual($this->Instit->isCUEValid("0201254"),1);
-		$this->assertEqual($this->Instit->isCUEValid("2601254"),1);
-		$this->assertEqual($this->Instit->isCUEValid("9401254"),1);
-		
-		// Estos son casos que fallan
-		$this->assertEqual($this->Instit->isCUEValid("2ss54"),-1);		
-		$this->assertEqual($this->Instit->isCUEValid("j.kjas"),-1);
 
-		$this->assertEqual($this->Instit->isCUEValid("501254"),-6);
-		$this->assertEqual($this->Instit->isCUEValid("9810125"),-7);
-		$this->assertEqual($this->Instit->isCUEValid("05101255"),-8);
-		$this->assertEqual($this->Instit->isCUEValid("051012555"),-9);
-		
-		// este es un numero intermedio entre 3 y 6 digitos
-		$this->assertEqual($this->Instit->isCUEValid("6542"),2);
-		
-		//este es un numero menor a 3 digitos
-		$this->assertEqual($this->Instit->isCUEValid("54"),-1);
-	}
+    function testInstitSinPlanGetOrientacionSegunSusPlanes() {
+        $this->loadFixtures('Instit', 'Plan', 'Sector', 'Subsector', 'Orientacion');
+
+        $instit_id = 1;
+        $conditions = array('Plan.instit_id'=>$instit_id);
+        $planes = $this->Instit->Plan->find('all',array(
+                'conditions'=>$conditions));
+
+        // le borro todos los planes
+        foreach ($planes as $p) {
+            $this->Instit->Plan->del($p['Plan']['id']);
+        }
+
+        $cant = $this->Instit->Plan->find('count',array('conditions'=>$conditions));
+
+        if ($cant == 0) {
+            $orientacion = $this->Instit->getOrientacionSegunSusPlanes($instit_id);
+            $this->assertEqual($orientacion, -1); //sin orientacion
+        } else {
+            $this->fail("Se tenian que borrar los planes pero no se borraron");
+        }
+
+        //prueba una institucion que no existe
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(56456);
+        $this->assertEqual($orientacion, -1); //sin orientacion
+    }
+
+    function testGetOrientacionMixtaSegunSusPlanes() {
+        $this->loadFixtures('Instit', 'Plan', 'Sector', 'Subsector', 'Orientacion');
+
+        /*************** Para la Instit ID= 1 ****************************/
+        $plan['Plan'] = array(
+                'id'                => 7,
+                'instit_id'         => 1,
+                'oferta_id'         => 1,
+                'old_item'          => 0,
+                'norma'             => "",
+                'nombre'            => "Titulo en nada",
+                'perfil'            => "Algun Perfil",
+                'sector'            => "",
+                'duracion_hs'       => 3,
+                'duracion_semanas'  => 2,
+                'duracion_anios'    => 4,
+                'matricula'         => 100,
+                'observacion'       => "Alguna observacion",
+                'ciclo_alta'        => 2007,
+                'ciclo_mod'         => 0,
+                //'created' 		=> "2010-01-21 13:54:45",
+                //'modified' 		=> "2010-01-21 13:54:45",
+                'sector_id'         => 3, // Sector = Agropecuaria -> Orientacion = Agropecuaria
+                'subsector_id'      => 1,
+        );
+        $this->Instit->Plan->create();
+        $this->Instit->Plan->save($plan, false);
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(1);
+        $this->assertEqual($orientacion, 0); //orientacion mixta
+
+
+        /*************** Para la Instit ID= 2 ****************************/
+        $plan['Plan'] = array(
+                'id'                => 8,
+                'instit_id'         => 2,
+                'oferta_id'         => 1,
+                'old_item'          => 0,
+                'norma'             => "",
+                'nombre'            => "Titulo en nada",
+                'perfil'            => "Algun Perfil",
+                'sector'            => "",
+                'duracion_hs'       => 3,
+                'duracion_semanas'  => 2,
+                'duracion_anios'    => 4,
+                'matricula'         => 100,
+                'observacion'       => "Alguna observacion",
+                'ciclo_alta'        => 2007,
+                'ciclo_mod'         => 0,
+                'sector_id'         => 4, //Sector Automotriz -- Orientacion = Industrial
+                'subsector_id'      => 1,
+        );
+        $this->Instit->Plan->create();
+        $this->Instit->Plan->save($plan, false);
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(2);
+        $this->assertEqual($orientacion, 0); //orientacion mixta
+
+        $this->Instit->Plan->del(8);
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(2);
+        $this->assertEqual($orientacion, 3);
+    }
+
+
+    function testGetOrientacionSegunSusPlanes() {
+        $this->loadFixtures('Instit', 'Plan', 'Sector', 'Subsector', 'Orientacion');
+
+        /*************** Para la Instit ID= 1 ****************************/
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(1);
+        $this->assertEqual($orientacion, 3); //orientacion otros
+
+        /* ************** Para la Instit ID= 2 *************************** */
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(2);
+        $this->assertEqual($orientacion, 3);
+
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(3);
+        $this->assertEqual($orientacion, 2);
+
+        $orientacion = $this->Instit->getOrientacionSegunSusPlanes(4);
+        $this->assertEqual($orientacion, 1);
+    }
+
+    function testIsCUEValid() {
+        // casos que deberian dar 1, porque todo paso bien
+        $this->assertEqual($this->Instit->isCUEValid("601254"),1);
+        $this->assertEqual($this->Instit->isCUEValid("201254"),1);
+        $this->assertEqual($this->Instit->isCUEValid("0601254"),1);
+        $this->assertEqual($this->Instit->isCUEValid("0201254"),1);
+        $this->assertEqual($this->Instit->isCUEValid("2601254"),1);
+        $this->assertEqual($this->Instit->isCUEValid("9401254"),1);
+
+        // Estos son casos que fallan
+        $this->assertEqual($this->Instit->isCUEValid("2ss54"),-1);
+        $this->assertEqual($this->Instit->isCUEValid("j.kjas"),-1);
+
+        $this->assertEqual($this->Instit->isCUEValid("501254"),-6);
+        $this->assertEqual($this->Instit->isCUEValid("9810125"),-7);
+        $this->assertEqual($this->Instit->isCUEValid("05101255"),-8);
+        $this->assertEqual($this->Instit->isCUEValid("051012555"),-9);
+
+        // este es un numero intermedio entre 3 y 6 digitos
+        $this->assertEqual($this->Instit->isCUEValid("6542"),2);
+
+        //este es un numero menor a 3 digitos
+        $this->assertEqual($this->Instit->isCUEValid("54"),-1);
+    }
 
 }
 ?>
