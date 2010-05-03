dc7ac1e51b784882e4796906b9ceeceb1e3f04d5
pablo.de.alpha <pablo.de.alpha@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-05-03 14:22:59 +0000

    validaciones para fondos_temp

--- app/tests/cases/models/fondo_temporal.test.php
+++ app/tests/cases/models/fondo_temporal.test.php
@@ -5,7 +5,16 @@ App::import('Model', 'FondoTemporal');
 
 class FondotemporalTestCase extends CakeTestCase {
     var $FondoTemporal = null;
-    var $fixtures = array('app.fondo', 'app.instit', 'app.jurisdiccion');
+
+    var $fixtures = array(
+            'app.z_fondo_work', 'app.jurisdiccion', 'app.instit', 'app.claseinstit',
+            'app.orientacion',  'app.sector', 'app.plan', 'app.subsector',
+            'app.lineas_de_accion', 'app.fondos_lineas_de_accion',
+            'app.tipoinstit', 'app.dependencia', 'app.departamento', 'app.localidad',
+            'app.etp_estado', 'app.oferta', 'app.titulo', 'app.anio', 'app.ciclo',
+            'app.etapa', 'app.gestion', 'app.historial_cue', 'app.ticket', 'app.user',
+            'app.user_login', 'app.fondo',
+    );
 
     function startTest() {
         /*
@@ -18,25 +27,80 @@ class FondotemporalTestCase extends CakeTestCase {
         $this->assertTrue(is_a($this->FondoTemporal, 'FondoTemporal'));
     }
 
-    function testFondoFind() {
-        $this->FondoTemporal->recursive = -1;
-        $results = $this->FondoTemporal->find('first');
-        $this->assertTrue(!empty($results));
+    function testCompara_numeroInstit() {
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N° 63','63');
+        $this->assertTrue($t1);
 
-        $expected = array('FondoTemporal' => array(
-                        'id' => 1,
-                        'instit_id' => 1,
-                        'jurisdiccion_id' => 1,
-                        'total' => 1,
-                        'anio' => 1,
-                        'trimestre' => 1,
-                        'memo' => 'Lorem ipsum dolor ',
-                        'resolucion' => 'Lorem ipsum dolor ',
-                        'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida,phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam,vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit,feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-                        'created' => '2010-04-22 10:25:00',
-                        'modified' => '2010-04-22 10:25:00'
-        ));
-        $this->assertEqual($results, $expected);
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N\' 63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N°63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N|63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N° 73','63');
+        $this->assertFalse($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('ET- Agro - Snopek','63');
+        $this->assertTrue($t1);
     }
+
+    function testCompara_tipoInstit() {
+        $tiposInstit = array('Tipoinstit'=>array(
+                    'id' => 33,
+                    'jurisdiccion_id' => 2,
+                    'name' => 'ESCUELA DE EDUCACIÓN TÉCNICA (E.E.T.)'
+                ),
+            array(
+                    'id' => 9,
+                    'jurisdiccion_id' => 2,
+                    'name' => 'CENTRO EDUCATIVO DE NIVEL TERCIARIO (C.E.N.T.)'
+                ),
+            array(
+                    'id' => 3,
+                    'jurisdiccion_id' => 2,
+                    'name' => 'ESCUELA POLITÉCNICA'
+                ),
+            array(
+                    'id' => 8,
+                    'jurisdiccion_id' => 2,
+                    'name' => 'ESCUELA'
+                )
+        );
+        $instit = array('FondoTemporal'=>array(
+		'id' => 2,
+		'anio' => 2009,
+		'trimestre' => 1,
+		'jurisdiccion_id' => 2,
+		'jurisdiccion_name' => 'CABA',
+		'memo' => 'xx',
+		'cuecompleto' => '24567801',
+		'instit' => 'I.P.E.M. Nº 63 REPÚBLICA DE ITALIA',
+		'instit_name' => '',
+		'departamento' => 'Lorem ipsum dolor sit amet',
+		'localidad' => 'Lorem ipsum dolor sit amet'
+        ));
+
+        $t1 = $this->FondoTemporal->testCompara_tipoInstit('BLA N° 63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N\' 63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N°63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N|63','63');
+        $this->assertTrue($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('BLA N° 73','63');
+        $this->assertFalse($t1);
+
+        $t1 = $this->FondoTemporal->compara_numeroInstit('ET- Agro - Snopek','63');
+        $this->assertTrue($t1);
+    }
+    
 }
 ?>
