9f40b625a86881f5d77b130d45f072984ccccd90
alevilar <alevilar@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-06-02 14:43:37 +0000

    cambio de version

--- trunk/app/app_controller.php
+++ trunk/app/app_controller.php
@@ -67,7 +67,7 @@ class AppController extends Controller {
 		 *  REGETP VERSION
 		 * 
 		 */
-		Configure::write('regetpVersion', '1.4');
+		Configure::write('regetpVersion', '1.4.1');
 
 		$this->Auth->autoRedirect = false; 
 		$this->Auth->loginError ='Usuario o Contraseña Incorrectos';
