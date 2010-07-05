a427c7ec199c8cbd642c59fd930b802969c38c47
alevilar <alevilar@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-07-05 13:40:46 +0000

    mejoras en las busquedas

--- app/models/jurisdiccion.php
+++ app/models/jurisdiccion.php
@@ -4,32 +4,7 @@ class Jurisdiccion extends AppModel {
 	var $name = 'Jurisdiccion';
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
-	var $hasMany = array(
-			'Tipoinstit' => array('className' => 'Tipoinstit',
-								'foreignKey' => 'jurisdiccion_id',
-								'dependent' => false,
-								'conditions' => '',
-								'fields' => '',
-								'order' => '',
-								'limit' => '',
-								'offset' => '',
-								'exclusive' => '',
-								'finderQuery' => '',
-								'counterQuery' => ''
-			),
-			'Instit' => array('className' => 'Instit',
-								'foreignKey' => 'jurisdiccion_id',
-								'dependent' => false,
-								'conditions' => '',
-								'fields' => '',
-								'order' => '',
-								'limit' => '',
-								'offset' => '',
-								'exclusive' => '',
-								'finderQuery' => '',
-								'counterQuery' => ''
-			)
-	);
+	var $hasMany = array('Tipoinstit','Instit','Departamento');
 	
 	var $validate = array(
       	'name' => array(
