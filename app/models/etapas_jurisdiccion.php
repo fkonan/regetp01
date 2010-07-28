30dea319c4a4ddb48f5e938986f2aa4104c615c6
pablo.de.alpha <pablo.de.alpha@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-07-28 16:19:42 +0000

    model para etapas_jurisdiccion

--- app/models/etapas_jurisdiccion.php
+++ app/models/etapas_jurisdiccion.php
@@ -6,7 +6,6 @@ class EtapasJurisdiccion extends AppModel {
 	var $maxCiclo = "";
 	var $traerUltimaAct = false; // se utiliza en el paginador.
 	
-	var $actsAs = array('Containable');
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $belongsTo = array( 
