9de848e9df279fc2003a5d4e22980547e70781ca
hernangigena <hernangigena@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-07-29 17:27:26 +0000

    agregar etapas por jurisdiccion

---
+++ trunk/app/controllers/etapas_jurisdicciones_controller.php
@@ -0,0 +1,105 @@
+<?php
+class EtapasJurisdiccionesController extends AppController {
+
+	var $name = 'EtapasJurisdicciones';
+	var $helpers = array('Html', 'Form');
+
+	function index($id = null) {
+
+                if (!empty($this->data['EtapasJurisdiccion'])) {
+                    $this->EtapasJurisdiccion->deleteAll(array('EtapasJurisdiccion.jurisdiccion_id ='. $this->data['EtapasJurisdiccion']['jurisdiccion_id']));
+                    if(!empty($this->data['EtapasJurisdiccion']['etapas_selected'])){
+                        foreach($this->data['EtapasJurisdiccion']['etapas_selected'] as $etapa){
+                            $this->EtapasJurisdiccion->create();
+                            $etapaJur = array("EtapasJurisdiccion"=>array("etapa_id"=>$etapa, "jurisdiccion_id"=>$this->data['EtapasJurisdiccion']['jurisdiccion_id']));
+                            $this->EtapasJurisdiccion->save($etapaJur);
+                        }
+                    }
+                    $this->redirect(array('action' => 'index',$this->data['EtapasJurisdiccion']['jurisdiccion_id']));
+                }
+
+                $notIn = array();
+                $this->EtapasJurisdiccion->recursive = 0;
+
+                $conditions = array('contain'=> array('Etapa'),
+                                    'conditions'=> array("jurisdiccion_id" => $id),
+                                    'fields' => array('DISTINCT Etapa.id', 'Etapa.name'));
+                
+                $etapasSeleccionadas = $this->EtapasJurisdiccion->find('all',$conditions);
+
+
+                if(!empty($etapasSeleccionadas)){
+                    foreach($etapasSeleccionadas as $etapa){
+                        array_push($notIn, $etapa['Etapa']['id']);
+                    }
+
+                    $conditions = array('contain'=> array('Etapa'),
+                                        'conditions'=> array('NOT'=>array("Etapa.id" => $notIn)),
+                                        'fields' => array('DISTINCT Etapa.id', 'Etapa.name'));
+
+                }
+                else{
+                    $conditions = array('contain'=> array('Etapa'),
+                                    'fields' => array('DISTINCT Etapa.id', 'Etapa.name'));
+                }
+                
+                $etapasNoSeleccionadas = $this->EtapasJurisdiccion->find('all',$conditions);
+                       
+		$this->set('etapasSeleccionadas', $etapasSeleccionadas);
+                $this->set('etapasNoSeleccionadas', $etapasNoSeleccionadas);
+                $this->set('jurisdiccion_id', $id);
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid EtapasJurisdicciones', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('etapasJurisdicciones', $this->EtapasJurisdicciones->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->EtapasJurisdicciones->create();
+			if ($this->EtapasJurisdicciones->save($this->data)) {
+				$this->Session->setFlash(__('The EtapasJurisdicciones has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The EtapasJurisdicciones could not be saved. Please, try again.', true));
+			}
+		}
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid EtapasJurisdicciones', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->EtapasJurisdicciones->save($this->data)) {
+				$this->Session->setFlash(__('The EtapasJurisdicciones has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The EtapasJurisdicciones could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->EtapasJurisdicciones->read(null, $id);
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for EtapasJurisdicciones', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if ($this->EtapasJurisdicciones->del($id)) {
+			$this->Session->setFlash(__('EtapasJurisdicciones deleted', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('The EtapasJurisdicciones could not be deleted. Please, try again.', true));
+		$this->redirect(array('action' => 'index'));
+	}
+
+}
+?>
