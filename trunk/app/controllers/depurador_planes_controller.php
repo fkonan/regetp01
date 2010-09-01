dbf8b6b8801f06a141c7db7edf54785da8100cd8
pablo.de.alpha <pablo.de.alpha@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-09-01 16:05:05 +0000

    estructura para el DEPURADOR DE PLANES

---
+++ trunk/app/controllers/depurador_planes_controller.php
@@ -0,0 +1,18 @@
+<?php
+set_time_limit(30000000);
+
+class DepuradorPlanesController extends AppController {
+
+	var $name = 'DepuradorPlanes';
+	var $helpers = array('Html', 'Form','Ajax');
+	var $uses = array('Instit','Plan','Anio','Sector','Jurisdiccion',
+                    'EstructuraPlan','JurisdiccionesEstructuraPlan','EstructuraPlanesAnio');
+	var $db;
+	
+	
+	function index() {
+
+        }
+}
+
+?>
