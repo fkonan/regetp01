71f96d3bc37afa1851326a2105f9966e66349f87
alevilar <alevilar@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-09-02 16:07:38 +0000

    - el script depurador ahora inserta la estructura del anio sugerida en z_estructura_anios de la tabla anios
    - nueva funcion que trae las estructuras para determinada jurisdiccion y se le puede pasar como parametro 'all' o 'list'

--- trunk/app/models/jurisdicciones_estructura_plan.php
+++ trunk/app/models/jurisdicciones_estructura_plan.php
@@ -19,5 +19,43 @@ class JurisdiccionesEstructuraPlan extends AppModel {
 			)
 	);
 
+
+        /**
+         *
+         *  Me devuelve todas las EstructuraPlan disponibles para una determinada
+         *  jurisdiccion.
+         *
+         * 
+         * @param integer $jurisdiccion_id ID de la jurisdiccion donde quiero buscar las estructuras
+         * @param string $find_type las posibilidades son:
+         *                                                  'all'
+         *                                                  'list'
+         * @return array del model 'EstructuraPlan' encontradas
+         */
+        function getEstructurasDeJurisdiccion($jurisdiccion_id, $find_type = 'all') {
+             $trayecto_anios = $this->find('all', array(
+                'fields' => array('EstructuraPlan.name'),
+                'contain' => array(
+                    'EstructuraPlan.EstructuraPlanesAnio',
+                ),
+                'conditions'=> array(
+                    'JurisdiccionesEstructuraPlan.jurisdiccion_id' => $jurisdiccion_id,
+                )));
+
+             // si es del tipo list convierto el resultado a ese formato
+             if ($find_type == 'list') {
+                 $nuevoTA = array();
+                 foreach ($trayecto_anios as $ta) {
+                    $estrucID = $ta['EstructuraPlan']['id'];
+                    $estrucNAME = $ta['EstructuraPlan']['name'];
+                    $nuevoTA[ $estrucID ] = $estrucNAME;
+                 }
+                 $trayecto_anios = $nuevoTA;
+
+             }
+
+            return $trayecto_anios;
+        }
+
 }
 ?>
