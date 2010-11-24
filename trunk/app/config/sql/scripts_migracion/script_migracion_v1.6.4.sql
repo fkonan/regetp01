817d0f767588b0cb93a0bef9a5bd6e771fdb80b7
hernan2003a@gmail.com <hernan2003a@gmail.com@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-11-24 16:27:25 +0000

    Se agrego un nuevo script de migracion del modelo sql con insert

--- trunk/app/config/sql/scripts_migracion/script_migracion_v1.6.4.sql
+++ trunk/app/config/sql/scripts_migracion/script_migracion_v1.6.4.sql
@@ -6,6 +6,14 @@ subsector_id integer not null default 0,
 prioridad integer not null default 0
 ) with oids;
 
-INSERT INTO sectores_titulos SELECT nextval('sectores_titulos_id_seq'::regclass) as id, sector_id , subsector_id , titulo_id from planes
-GROUP BY sector_id , subsector_id , titulo_id
+INSERT INTO sectores_titulos
+SELECT nextval('sectores_titulos_id_seq'::regclass) as id,
+titulo_id ,
+sector_id ,
+subsector_id
+FROM planes
+GROUP BY
+sector_id ,
+subsector_id ,
+titulo_id
 ;
