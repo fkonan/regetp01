ecf69c47c456b3eb97e4ffdfa838ef6d941e8021
pablo.de.alpha <pablo.de.alpha@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2010-11-29 16:29:22 +0000

    correccion en nombre de SectoresTitulo

--- trunk/app/models/sector.php
+++ trunk/app/models/sector.php
@@ -9,7 +9,7 @@ class Sector extends AppModel {
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $hasMany = array(
-                        'Sectores_titulo',
+                        'SectoresTitulo',
 			'Plan' => array('className' => 'Plan',
 								'foreignKey' => 'sector_id',
 								'dependent' => false,
