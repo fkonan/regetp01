3e33dacf8cb27f6b1a8960d6ac6786a4081ad0cc
pablo.de.alpha <pablo.de.alpha@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2011-04-07 12:43:25 +0000

    checkPasswords actualizado con ultimo referente

--- trunk/app/controllers/utils_controller.php
+++ trunk/app/controllers/utils_controller.php
@@ -375,6 +375,17 @@ class UtilsController extends AppController {
                     }
                     echo " (último login: ".date('d/m/Y H:i', strtotime($user['UserLogin'][0]['created'])).")";
                     break;
+
+                case "sreyes":
+                    echo "[".$user['User']['id']."] sreyes: ";
+                    if ($this->Auth->password("mo92na") != $user['User']['password']) {
+                        echo "cambió password";
+                    }
+                    else {
+                        echo "<b>no cambió password</b>";
+                    }
+                    echo " (último login: ".date('d/m/Y H:i', strtotime($user['UserLogin'][0]['created'])).")";
+                    break;
             }
             
             echo "<br /><br />";
