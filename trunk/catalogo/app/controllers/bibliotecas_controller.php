d00a52ebedfc4feb42604ec1dee00150e6aab6f3
alevilar <alevilar@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2011-05-02 15:57:44 +0000

    controller

--- trunk/catalogo/app/controllers/bibliotecas_controller.php
+++ trunk/catalogo/app/controllers/bibliotecas_controller.php
@@ -6,12 +6,18 @@ class BibliotecasController extends AppController{
     // sin tablas asociadas
     var $uses = array();
     
+    var $helpers = array('ArrayWritter');
+    
+    var $components = array('FileReader');
+    
     function index(){
-        $path =  APP_DIR . DS . WEBROOT_DIR . DS .'files/pdfs/marcos_referencia';
-        $fol = new Folder($path);
-        $marcos_referencia = $fol->read();
-        debug($fol->pwd());
-        debug($marcos_referencia);
-        $this->set(compact('marcos_referencia'));
+        
+        $this->cacheAction = true;
+        
+        $pdfs_path = WWW_ROOT . 'files' . DS. 'pdfs' . DS;       
+        
+        $this->set('archivos', $this->FileReader->getFiles($pdfs_path));
+        
+        $this->set('archivos2', $this->FileReader->getFiles($pdfs_path.DS.'resoluciones'));
     }
 }
