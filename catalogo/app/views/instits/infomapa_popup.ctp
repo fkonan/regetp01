f2d02a9867997df1e50ad6eff9c88ed484aceba6
pablo.de.alpha <pablo.de.alpha@gmail.com>
2011-08-29 13:08:14 +0000

    Catalogo > seccion mapas: matriculas reales! se creó tabla "matriculados" con sus datos en database_cleanup.sql

--- catalogo/app/views/instits/infomapa_popup.ctp
+++ catalogo/app/views/instits/infomapa_popup.ctp
@@ -4,7 +4,7 @@
             <strong><?php __('Instituciones registradas'); ?>:</strong> <?php echo $total ?>
         </li>
         <li class="">
-            <strong><?php __('Alumnos matriculados'); ?>:</strong> <?php echo $total ?>
+            <strong><?php __('Alumnos matriculados'); ?>:</strong> <?php echo $matriculados ?>
         </li>
         <li class="">
             <strong><?php __('Gestión estatal'); ?>:</strong> <?php echo $promedio ?>%
--- catalogo/database_cleanup.sql
+++ catalogo/database_cleanup.sql
@@ -69,5 +69,97 @@ insert into users(username, nombre, apellido, "password", mail, oficina, interno
 		  "role", jurisdiccion_id, created, modified)
             values('invitado', 'invitado', 'invitado','0154538b07449bae7673573bda20011340894a2e', 
 		   'invitado@test.com', '','','invitados',0,now(), now());
+
+/* Tabla de matriculados harcodeados */
+CREATE TABLE matriculados
+(
+   id serial, 
+   jurisdiccion_id integer NOT NULL, 
+   oferta_id integer NOT NULL, 
+   cantidad bigint DEFAULT 0, 
+    PRIMARY KEY (id)
+) 
+WITH (
+  OIDS = FALSE
+)
+;
+
+/* FP */
+INSERT INTO matriculados VALUES (5, 6, 1, 95137);
+INSERT INTO matriculados VALUES (6, 10, 1, 4965);
+INSERT INTO matriculados VALUES (7, 22, 1, 9706);
+INSERT INTO matriculados VALUES (8, 26, 1, 3047);
+INSERT INTO matriculados VALUES (9, 2, 1, 17811);
+INSERT INTO matriculados VALUES (10, 14, 1, 6224);
+INSERT INTO matriculados VALUES (11, 18, 1, 4992);
+INSERT INTO matriculados VALUES (12, 30, 1, 7364);
+INSERT INTO matriculados VALUES (13, 34, 1, 6924);
+INSERT INTO matriculados VALUES (14, 38, 1, 5320);
+INSERT INTO matriculados VALUES (15, 42, 1, 1681);
+INSERT INTO matriculados VALUES (16, 46, 1, 1841);
+INSERT INTO matriculados VALUES (17, 50, 1, 14294);
+INSERT INTO matriculados VALUES (18, 54, 1, 2330);
+INSERT INTO matriculados VALUES (19, 58, 1, 10464);
+INSERT INTO matriculados VALUES (20, 62, 1, 1822);
+INSERT INTO matriculados VALUES (21, 66, 1, 4140);
+INSERT INTO matriculados VALUES (22, 70, 1, 12907);
+INSERT INTO matriculados VALUES (23, 74, 1, 0);
+INSERT INTO matriculados VALUES (24, 78, 1, 593);
+INSERT INTO matriculados VALUES (25, 82, 1, 6018);
+INSERT INTO matriculados VALUES (26, 86, 1, 7271);
+INSERT INTO matriculados VALUES (27, 94, 1, 399);
+INSERT INTO matriculados VALUES (28, 90, 1, 10406);
+/* SEC */
+INSERT INTO matriculados VALUES (29, 6, 3, 173540);
+INSERT INTO matriculados VALUES (30, 10, 3, 4799);
+INSERT INTO matriculados VALUES (31, 22, 3, 11883);
+INSERT INTO matriculados VALUES (32, 26, 3, 14471);
+INSERT INTO matriculados VALUES (33, 2, 3, 45014);
+INSERT INTO matriculados VALUES (34, 14, 3, 80339);
+INSERT INTO matriculados VALUES (35, 18, 3, 12109);
+INSERT INTO matriculados VALUES (36, 30, 3, 21449);
+INSERT INTO matriculados VALUES (37, 34, 3, 7455);
+INSERT INTO matriculados VALUES (38, 38, 3, 9381);
+INSERT INTO matriculados VALUES (39, 42, 3, 2726);
+INSERT INTO matriculados VALUES (40, 46, 3, 7341);
+INSERT INTO matriculados VALUES (41, 50, 3, 32370);
+INSERT INTO matriculados VALUES (42, 54, 3, 15184);
+INSERT INTO matriculados VALUES (43, 58, 3, 13002);
+INSERT INTO matriculados VALUES (44, 62, 3, 9767);
+INSERT INTO matriculados VALUES (45, 66, 3, 24040);
+INSERT INTO matriculados VALUES (46, 70, 3, 13126);
+INSERT INTO matriculados VALUES (47, 74, 3, 8823);
+INSERT INTO matriculados VALUES (48, 78, 3, 2351);
+INSERT INTO matriculados VALUES (49, 82, 3, 60808);
+INSERT INTO matriculados VALUES (50, 86, 3, 14353);
+INSERT INTO matriculados VALUES (51, 94, 3, 4019);
+INSERT INTO matriculados VALUES (52, 90, 3, 22549);
+/* SUP */
+INSERT INTO matriculados VALUES (53, 6, 4, 23929);
+INSERT INTO matriculados VALUES (54, 10, 4, 1951);
+INSERT INTO matriculados VALUES (55, 22, 4, 2703);
+INSERT INTO matriculados VALUES (56, 26, 4, 947);
+INSERT INTO matriculados VALUES (57, 2, 4, 46951);
+INSERT INTO matriculados VALUES (58, 14, 4, 26125);
+INSERT INTO matriculados VALUES (59, 18, 4, 4118);
+INSERT INTO matriculados VALUES (60, 30, 4, 1984);
+INSERT INTO matriculados VALUES (61, 34, 4, 1878);
+INSERT INTO matriculados VALUES (62, 38, 4, 3777);
+INSERT INTO matriculados VALUES (63, 42, 4, 1068);
+INSERT INTO matriculados VALUES (64, 46, 4, 572);
+INSERT INTO matriculados VALUES (65, 50, 4, 5633);
+INSERT INTO matriculados VALUES (66, 54, 4, 6557);
+INSERT INTO matriculados VALUES (67, 58, 4, 2255);
+INSERT INTO matriculados VALUES (68, 62, 4, 1318);
+INSERT INTO matriculados VALUES (69, 66, 4, 4242);
+INSERT INTO matriculados VALUES (70, 70, 4, 2442);
+INSERT INTO matriculados VALUES (71, 74, 4, 0);
+INSERT INTO matriculados VALUES (72, 78, 4, 0);
+INSERT INTO matriculados VALUES (73, 82, 4, 26424);
+INSERT INTO matriculados VALUES (74, 86, 4, 628);
+INSERT INTO matriculados VALUES (75, 94, 4, 1602);
+INSERT INTO matriculados VALUES (76, 90, 4, 9713);
+
+
 /*rollback;*/
 commit;
