1acdeeeae2a3860276a8eb27bb04d782c18637f8
alevilar <alevilar@gmail.com>
2011-09-02 14:07:01 +0000

    reordenamiento de  la documentacion. Se agregaron los marcos de referencia. Faltarian poner los pdfs en cada carpeta

--- catalogo/app/views/helpers/file_structure_writter.php
+++ catalogo/app/views/helpers/file_structure_writter.php
@@ -28,6 +28,34 @@ class FileStructureWritterHelper extends AppHelper {
     var $paths = '';
     var $level = 1;
     
+    function write($carpeta){
+        return $this->getFileStructure( $this->getFiles($carpeta) );
+    }
+    
+    function tieneArchivos($carpeta){
+        return count($this->getFiles($carpeta)) > 0;
+    }
+    
+    function getFiles($carpeta, &$vArchivos = array()){
+        // agarro el nombre de la carpeta que estoy leyendo
+        $carpetaSplitted = split(DS, $carpeta);
+        $folderName = array_pop($carpetaSplitted);
+        
+        $fol =& new Folder($carpeta);
+        $dir = $fol->read(true, true);
+        $subfolders = $dir[0];
+        $files = $dir[1];
+        $vArchivos[$folderName]['folders'] = array();
+        foreach ( $subfolders as $sf) {
+            $this->getFiles($carpeta . DS . $sf, &$vArchivos[$folderName]['folders']);
+        }
+                
+        $vArchivos[$folderName]['files'] = $files; // 1 lee solo los archivos (creo) no lee las carpetas
+        
+        return $vArchivos;
+    }   
+
+    
     function getFileStructure($array) {
 
         $mainkey = key($array);
