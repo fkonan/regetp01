2fc75abc6daed3fea50c07ca10dcf7e58e11a106
pablo.de.alpha@gmail.com <pablo.de.alpha@gmail.com@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2011-09-12 19:46:19 +0000

    autocompletes con jquery ui integrados!

--- trunk/catalogo/app/webroot/js/views/titulos/search.js
+++ trunk/catalogo/app/webroot/js/views/titulos/search.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
+(function($){    
+//$(document).ready(function(){
+    if($('#jurisdiccion_id').val() != 0){
+        $depto_aux = $('#departamento_id').val();
+        reloadCombo($('#jurisdiccion_id').val(), $depto_aux);
+    }
+
     $('#SectorId').change(function(){
         var url = urlDomain+'/subsectores/ajax_select_subsector_form_por_sector';
         $.post(url,{'data[sector_id]':$(this).val()},function(data){
@@ -6,43 +12,38 @@ $(document).ready(function(){
         })
     });
     
-    if($('#jurisdiccion_id').val() != 0){
-        $depto_aux = $('#departamento_id').val();
-        reloadCombo($('#jurisdiccion_id').val(), $depto_aux);
-    }
-
-    $('#jurisdiccion_id').change(function(){
+    $('#jurisdiccion_id').live("change",function(event){
         reloadCombo($(this).val());
         $("#ajax_indicator").hide();
         $('#LocalidadName').val('');
         $("#hiddenLocId").val('');
     });
 
-    $('#departamento_id').change(function(){
+    $('#departamento_id').live("change",function(event){
         $("#ajax_indicator").hide();
         $('#LocalidadName').val('');
         $("#hiddenLocId").val('');
     });
 
-    $("#LocalidadName").change(function(){
+    $("#LocalidadName").live("keyup",function(event){
         if($("#LocalidadName").val().length == 0){
             $("#hiddenLocId").val('');
+            $("#jurisdiccion_id").val('');
+            $("#departamento_id").val('');
         }
     });
-});
     
 
-function reloadCombo(jur, depto){
-    var url = urlDomain+'/departamentos/ajax_select_departamento_form_por_jurisdiccion';
-    $.post(url,{'data[jurisdiccion_id]':jur},function(data){
-        $('#departamento_id').html(data);
-        $('#departamento_id').val(depto);
-    })
-}
+    function reloadCombo(jur, depto){
+        var url = urlDomain+'/departamentos/ajax_select_departamento_form_por_jurisdiccion';
+        $.post(url,{'data[jurisdiccion_id]':jur},function(data){
+            $('#departamento_id').html(data);
+            $('#departamento_id').val(depto);
+        })
+    }
 
-function formatResult(loc_dep) {
-    return loc_dep.localidad;
-}
+}(jQuery));
+
 
 function init__SearchFormJs(locUrl) {
     $( "#LocalidadName" ).autocomplete({
