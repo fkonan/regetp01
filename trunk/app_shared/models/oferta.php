60612e01f434e87993fe4915a5bc8c4c32e5d4bf
alevilar@gmail.com <alevilar@gmail.com@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2011-10-20 15:16:43 +0000

    se agrego el orden en la tabla de ofertas para registro

--- trunk/app_shared/models/oferta.php
+++ trunk/app_shared/models/oferta.php
@@ -3,7 +3,7 @@ class Oferta extends AppModel {
 
 	var $name = 'Oferta';
         
-        var $order = 'Oferta.name';
+        var $order = 'Oferta.order';
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $hasMany = array(
