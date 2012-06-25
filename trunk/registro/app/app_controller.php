f4d647da5d2eea9bccf596e72964707f80d73d49
pablo.de.alpha <pablo.de.alpha@e1a5d64a-0cb6-11de-a6e9-d320b29efae0>
2012-06-25 18:52:17 +0000

    textos de v.1.7.10

--- trunk/registro/app/app_controller.php
+++ trunk/registro/app/app_controller.php
@@ -67,7 +67,7 @@ class AppController extends Controller {
 		 *  REGETP VERSION
 		 * 
 		 */
-		Configure::write('regetpVersion', '1.7.9');
+		Configure::write('regetpVersion', '1.7.10');
                 /*
 		$this->Auth->autoRedirect = false;
 		$this->Auth->loginError ='Usuario o Contraseña Incorrectos';
